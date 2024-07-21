dc2a70a7ab50d8f0f0b02c4c5d7b54e03dea3e50
Eduardo Mendez <67878896+E0DUAR@users.noreply.github.com>
2024-07-21 18:33:10 -0500

    Funcionalidad de encriptación completa

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
-encrypt();
-decrypt();
-copytest();
+function encrypt() {
+    let text = document.getElementById('text-area').value; 
+    console.log(`Texto inicial: ${text}`);
+    
+    let encryptedText = text.replace(/a/g, 'ai')
+                            .replace(/e/g, 'enter')
+                            .replace(/i/g, 'imes')
+                            .replace(/o/g, 'ober')
+                            .replace(/u/g, 'ufat');
 
+    console.log(`Texto encriptado: ${encryptedText}`);
+}
+
+function decrypt() {
+    let text = document.getElementById('text-area').value;
+    console.log(`Texto encriptado: ${text}`);
+
+    let decryptedText = text.replace(/ufat/g, 'u')
+                            .replace(/ober/g, 'o')
+                            .replace(/imes/g, 'i')
+                            .replace(/enter/g, 'e')
+                            .replace(/ai/g, 'a');
+
+    console.log(`Texto desencriptado: ${decryptedText}`);
+}
