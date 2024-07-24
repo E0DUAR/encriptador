a5ae6c22438f0aca03ab0e281ddc13bc307ed29b
Eduardo Mendez <67878896+E0DUAR@users.noreply.github.com>
2024-07-23 19:54:40 -0500

    Finalización Código Funcional

--- js/app.js
+++ js/app.js
@@ -1,33 +1,99 @@
 function encrypt() {
-    let text = document.getElementById('text-area').value;
+    const textArea = document.getElementById("text-area");
+    const rightContentStart = document.getElementById(
+      "cont-right-child-content-start"
+    );
+    const rightContentEnd = document.getElementById(
+      "cont-right-child-content-end"
+    );
+    const textAreaRight = document.getElementById("text-area-right");
+  
+    const text = textArea.value;
+  
+    const isValid = /^[a-z\s]+$/.test(text);
+    if (!isValid) {
+      alert(
+        "El texto debe contener solo letras minúsculas sin acentos ni caracteres especiales."
+      );
+      textArea.value = ""; 
+      return;
+    }
+  
     console.log(`Texto inicial: ${text}`);
-
-    document.getElementById('cont-right-child-content-start').style.display = 'none';
-    document.getElementById('cont-right-child-content-end').style.display = 'block';
-
-    let encryptedText = text.replace(/a/g, 'ai')
-                            .replace(/e/g, 'enter')
-                            .replace(/i/g, 'imes')
-                            .replace(/o/g, 'ober')
-                            .replace(/u/g, 'ufat');
-
+  
+    hideDiv(rightContentStart);
+    showDiv(rightContentEnd);
+  
+    const encryptedText = text
+      .replace(/a/g, "ai")
+      .replace(/e/g, "enter")
+      .replace(/i/g, "imes")
+      .replace(/o/g, "ober")
+      .replace(/u/g, "ufat");
+  
     console.log(`Texto encriptado: ${encryptedText}`);
-
-    document.getElementById('text-area-right').value = encryptedText;
-}
-
-function decrypt() {
-    let encryptedText = document.getElementById('text-area-right').value;
-
+    textAreaRight.value = encryptedText;
+  }
+  
+  
+  
+  function decrypt() {
+    const textArea = document.getElementById("text-area");
+    const textAreaRight = document.getElementById("text-area-right"); 
+    const encryptedText = textArea.value || textAreaRight.value; // Usar el texto del textarea izquierdo o derecho
+  
+    const isValid = /^[a-z\saienterimesoberufat]+$/.test(encryptedText);
+    if (!isValid) {
+      alert(
+        "El texto debe contener solo letras minúsculas sin acentos ni caracteres especiales."
+      );
+      textAreaRight.value = "";
+      return;
+    }
+  
     console.log(`Texto encriptado: ${encryptedText}`);
-
-    let decryptedText = encryptedText.replace(/ufat/g, 'u')
-                            .replace(/ober/g, 'o')
-                            .replace(/imes/g, 'i')
-                            .replace(/enter/g, 'e')
-                            .replace(/ai/g, 'a');
-
+  
+    const decryptedText = encryptedText
+      .replace(/ufat/g, "u")
+      .replace(/ober/g, "o")
+      .replace(/imes/g, "i")
+      .replace(/enter/g, "e")
+      .replace(/ai/g, "a");
+  
     console.log(`Texto desencriptado: ${decryptedText}`);
-    
-    document.getElementById('text-area-right').value = decryptedText;
-}
+    textAreaRight.value = decryptedText;
+  }
+  
+  
+  
+  function copyText() {
+    const textAreaRight = document.getElementById("text-area-right");
+    const rightContentStart = document.getElementById(
+      "cont-right-child-content-start"
+    );
+    const rightContentEnd = document.getElementById(
+      "cont-right-child-content-end"
+    );
+  
+    textAreaRight.disabled = false;
+    textAreaRight.select();
+    textAreaRight.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert("Contenido copiado");
+    textAreaRight.disabled = true;
+  
+    showDiv(rightContentStart);
+    hideDiv(rightContentEnd);
+  }
+  
+  
+  
+  function hideDiv(element) {
+    element.style.display = "none";
+  }
+  
+  
+  function showDiv(element) {
+    element.style.display = "block";
+  }
+  
